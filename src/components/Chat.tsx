1e5cc0cb7a2d6cbc3d22f6fcde648ab446785bf6
dwestland <don@westland.net>
2023-05-07 14:47:59 -0700

    Complete functionality

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,8 +1,8 @@
-// 'use client'
+'use client'
 
 import { FC } from 'react'
 import ChatInput from './ChatInput'
-// import ChatMessages from './ChatMessages'
+import ChatMessages from './ChatMessages'
 import ChatHeader from './ChatHeader'
 import {
   Accordion,
@@ -25,8 +25,7 @@ const Chat: FC = () => {
             </AccordionTrigger>
             <AccordionContent>
               <div className='flex flex-col h-80'>
-                {/* <ChatMessages className='px-2 py-3 flex-1' /> */}
-                messages
+                <ChatMessages className='px-2 py-3 flex-1' />
                 <ChatInput className='px-4' />
               </div>
             </AccordionContent>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-// import { MessagesProvider } from '@/context/messages'
+import { MessagesProvider } from '@/context/messages'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { FC, ReactNode } from 'react'
 
@@ -13,9 +13,9 @@ const Layout: FC<LayoutProps> = ({ children }) => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <MessagesProvider> */}
+      <MessagesProvider>
         {children}
-      {/* </MessagesProvider> */}
+      </MessagesProvider>
     </QueryClientProvider>
   )
 }
